d155beb9205adba497567cd3dc67c51b053e51dd
Damla Parlakyıldız <dmlprlkyldz@gmail.com>
2024-12-15 14:37:58 +0300

    login screen updates

--- lib/widgets/entry_screen/first_screen.dart
+++ lib/widgets/entry_screen/first_screen.dart
@@ -1,18 +1,15 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'dart:io';
 
 import 'package:fa_reporter/main.dart';
 import 'package:fa_reporter/utils/user_getset.dart';
 import 'package:fa_reporter/widgets/data_entry/previous_data_view.dart';
 import 'package:fa_reporter/widgets/ocr/text_detector_view.dart';
-import 'package:flutter/material.dart'; // Import the PreviousDataView class
+import 'package:flutter/material.dart';
 
 class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _FirstScreenState createState() => _FirstScreenState();
 }
 
@@ -20,102 +17,159 @@ class _FirstScreenState extends State<FirstScreen> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController inventoryNumberController =
       TextEditingController();
-  String? selectedLocation; // Variable to hold the selected dropdown value
+  String? selectedLocation;
 
- @override
-Widget build(BuildContext context) {
-  return Scaffold(
-    resizeToAvoidBottomInset: true, // Allow resizing of the screen
-    appBar: AppBar(
-      title: Text("KanSay Rapor" // make the text bold and centered
-        ,style: TextStyle(
-          fontWeight: FontWeight.bold,
+  
+  bool isNameEmpty = false;
+  bool isInventoryEmpty = false;
+  bool isLocationEmpty = false;
+
+  void validateForm() {
+    setState(() {
+      isNameEmpty = nameController.text.isEmpty;
+      isInventoryEmpty = inventoryNumberController.text.isEmpty;
+      isLocationEmpty = selectedLocation == null;
+    });
+
+    
+    if (isNameEmpty || isInventoryEmpty || isLocationEmpty) {
+      return;
+    }
+
+    // Tüm alanlar doluysa, devam et
+    if (Platform.isAndroid) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => TextRecognizerView(),
         ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      appBar: AppBar(
+        title: Text(
+          "KanSay Rapor",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
       ),
-      centerTitle: true,
-    ),
-    body: SingleChildScrollView( // Add SingleChildScrollView here
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Hoş Geldiniz.",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20), // Spacer
-            Text("İsminiz:"),
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "İsminizi girin",
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Hoş Geldiniz.",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              onChanged: (value) => setUserName(value),
-            ),
-            SizedBox(height: 20), // Spacer
-            Text("Sayım No:"),
-            TextField(
-              controller: inventoryNumberController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "Sayım numarasını girin",
+              SizedBox(height: 20),
+              Text("İsminiz:"),
+              TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "İsminizi girin",
+                  errorText: isNameEmpty ? "Lütfen isminizi giriniz!" : null,
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    isNameEmpty = value.isEmpty;
+                  });
+                  setUserName(value);
+                },
               ),
-              keyboardType: TextInputType.number, // Numeric keyboard for numbers
-              onChanged: (value) => setTollNumber(value),
-            ),
-            SizedBox(height: 20), // Spacer
-            Text("Sayım Lokasyonunuzu Seçin:"),
-            DropdownButton<String>(
-              value: selectedLocation,
-              hint: Text("Seçin"),
-              isExpanded: true,
-              items: <String>[
-                'ARGE MÜDÜRLÜK',
-                'ARGE PERFORMANS',
-                'ARGE PME',
-                "ARGE POLİMER"
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedLocation = newValue; // Update selected location
+              SizedBox(height: 20),
+              Text("Sayım No:"),
+              TextField(
+                controller: inventoryNumberController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Sayım numarasını girin",
+                  errorText: isInventoryEmpty
+                      ? "Lütfen sayım numarasını giriniz!"
+                      : null,
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (value) {
+                  setState(() {
+                    isInventoryEmpty = value.isEmpty;
+                  });
+                  setTollNumber(value);
+                },
+              ),
+              SizedBox(height: 20),
+              Text("Sayım Lokasyonunuzu Seçin:"),
+              DropdownButtonFormField<String>(
+                value: selectedLocation,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  errorText:
+                      isLocationEmpty ? "Lütfen bir lokasyon seçiniz!" : null,
+                ),
+                hint: Text("Seçin"),
+                isExpanded: true,
+                items: <String>[
+                  'ARGE MÜDÜRLÜK',
+                  'ARGE PERFORMANS',
+                  'ARGE PME',
+                  "ARGE POLİMER"
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    selectedLocation = newValue;
+                    isLocationEmpty = newValue == null;
+                  });
                   setUserLocation(selectedLocation);
-                });
-              },
-            ),
-            SizedBox(height: 20), // Spacer
-            Text("Tarih: ${getUserCurrentDate()}"),
-            ExpansionTile(
-              title: const Text('Taramaya Başlayın',),
-              children: [
-                if (Platform.isAndroid)
-                  CustomCard(
-                      'Nesne Numarası Tarama', TextRecognizerView()),
-              ],
-            ),
-            const SizedBox(height: 20), // Spacer before the button
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => PreviousDataView()),
-                );
-              },
-              child: const Text('Önceki Sayımlarım'),
-            ),
-          ],
+                },
+              ),
+              SizedBox(height: 20),
+              Text("Tarih: ${getUserCurrentDate()}"),
+              ExpansionTile(
+                title: const Text(
+                  'Taramaya Başlayın',
+                ),
+                children: [
+                  ElevatedButton(
+                    onPressed: validateForm,
+                    child: const Text("Kamera ile Tarama"),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (nameController.text.isEmpty ||
+                      inventoryNumberController.text.isEmpty ||
+                      selectedLocation == null) {
+                    validateForm();
+                  } else {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PreviousDataView(),
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Önceki Sayımlarım'),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
+    );
   }
 }
-
-
